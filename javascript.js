78e0b94b919a5428233cd56ce9a088888fa3e432
Richard Davidian <127055926+rhdavidian@users.noreply.github.com>
2023-11-22 16:30:27 -0600

    Create functions and logic for calculations. Should have made more commits in between, took it one step at a time.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,90 @@
-function add(a, b) {
-    return a + b;
-};
 
+const display = document.querySelector('input');
+let equation = document.getElementById('equation');
+equation.textContent = '';
+let frstNumber = null;
+let operator = null;
+let secNumber = null;
+
+const clear = document.querySelector('#clear')
+    clear.addEventListener('click', () => {
+        display.value = '';
+        equation.textContent = '';
+        frstNumber = null;
+        secNumber = null;
+        operator = null;
+    });
+
+const btn = document.querySelectorAll('.number');
+    btn.forEach(button => {
+    button.addEventListener('click', () => {
+        display.value += button.textContent;
+        });
+    });
+
+const btnSign = document.querySelectorAll('.signs');
+    btnSign.forEach(button => {
+        button.addEventListener('click', () => {
+              //Dealing with operating on an existing answer
+
+            if (display.value == ''){
+                frstNumber = +equation.textContent;
+                operator = button.textContent;
+                equation.textContent += ' ' + operator;
+            } else if (frstNumber == null) {
+                frstNumber = display.value;
+                operator = button.textContent;
+                equation.textContent += display.value + ' ' + operator + ' ';
+                display.value = '';
+
+            } else if (secNumber == null) {
+                secNumber = display.value;
+                frstNumber = operate(frstNumber, operator, secNumber);
+                operator = button.textContent;
+                equation.textContent += display.value + ' ' + operator + ' ';
+                display.value = '';
+           
+            } else if (operator != null && frstNumber != null) {
+                secNumber = display.value;
+                frstNumber = operate(frstNumber, operator, secNumber);
+                operator = button.textContent;
+                equation.textContent += secNumber + ' ' + operator + ' ';
+                display.value = '';
+            }
+        });
+    });
+
+    const equals = document.getElementById('equals');
+        equals.addEventListener('click', () => {
+            if (secNumber == null) {
+                secNumber = display.value;
+                equation.textContent += display.value + '=';
+                display.value = '';
+            } else {
+                secNumber = display.value;
+                equation.textContent += display.value + '=';
+                display.value = '';
+            }
+            equation.textContent = operate(frstNumber, operator, secNumber);
+        });
+
+function add(a, b) {
+    return +a + +b;
+};
 function subtract(a, b){
     return a - b;
 };
-
 function multiply(a, b) {
     return a * b;
 };
-
 function divide(a, b) {
     return a / b;
 };
+
+function operate(num1, oper, num2) {
+    if (oper == '+') {return add(num1, num2)};
+    if (oper == '-') {return subtract(num1, num2)};
+    if (oper == 'x') {return multiply(num1, num2)};
+    if (oper == '÷') {return divide(num1, num2)};
+};
+
